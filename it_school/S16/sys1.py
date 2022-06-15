da741ca431df2b497791c538a390d12f707877bc
Gabriel.G <gabrielgutui8@gmail.com>
2022-06-15 17:20:25 +0300

    update

--- it_school/S16/sys1.py
+++ it_school/S16/sys1.py
@@ -1,4 +1,16 @@
+from datetime import datetime
 import sys
 
-# print(sys.maxsize)
+# oprire executie
+user_choice = int(input("Enter a number: "))
 
+
+if user_choice % 2 == 0:
+    print("Numar par")
+    print("Oprire program cu sys")
+    sys.exit(1)
+else:
+    print("NUmar impar")
+    print("Programul va continua")
+
+print(datetime.now())
